c125c1f8a37952788cbad0b28e37a1e7f7b365b9
Kevin Lee <leekevin@berkeley.edu>
2015-05-24 21:37:46 -0700

    Add a bad homepage

--- cas/urls.py
+++ cas/urls.py
@@ -18,4 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', include('journal.urls'))
+    # url(r'^$', views.home, name='home')
 ]
---
+++ journal/static/journal/js/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    $(".background").css("height", $(window).height() + "px");
+    fadeInHeader();
+    $("#categorizations").css("height", $(window).height() + "px");
+    linkSmoothScroll();
+});
+
+
+function fadeInHeader() {
+    $("#overlay").css("display", "none");
+    $("#main-title").css("display","none");
+    $("#small").css("display", "none");
+    $("#big").css("display", "none");
+    $("#greeter").css("display", "none");
+    setTimeout(function() {
+        $("#overlay").fadeIn(300);
+    }, 700);
+    setTimeout(function() {
+        $("#main-title").fadeIn(300);
+        $("#big").fadeIn(2000);
+        $("#small").fadeIn(2000);
+        $("#greeter").fadeIn(2000);
+    }, 2000);
+    setTimeout(function() {
+      $('#bouncing-arrow').show(500);
+  }, 3500);
+}
+
+function linkSmoothScroll() {
+    $("a[href^='#']").click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: $($.attr(this, 'href')).offset().top
+        }, 1000);
+    });
+}
