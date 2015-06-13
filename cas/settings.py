b2847177b56edd555060a66e6d824a57fdc00c8a
Kevin Lee <leekevin@berkeley.edu>
2015-06-13 13:23:10 -0700

    Remove stuff

--- cas/settings.py
+++ cas/settings.py
@@ -128,7 +128,6 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     os.path.join(BASE_DIR, "static"),
-    '/home/leekevin/cas/journal/static',
 )
 
 TEMPLATE_DIRS = (
