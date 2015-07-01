fb9e69da1f7d73f7ef3362ba1a70114f64afff78
Kevin Lee <leekevin@berkeley.edu>
2015-06-30 21:19:24 -0700

    alpha version of the entries

--- journal/models.py
+++ journal/models.py
@@ -74,14 +74,15 @@ class Entry(models.Model):
     last_modified = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
     entry_type = models.CharField(max_length=1, choices=ENTRY_TYPES)
-    if entry_type == 'T':
-        entry = models.TextField()
-    elif entry_type == 'I':
-        entry = models.ImageField()
-    elif entry_type == 'V':
-        entry = models.URLField()  # Web links to Youtube or else where
-    else:
-        entry = models.URLField()  # Web links to Youtube or else where
+    entry = models.TextField()
+    # if entry_type == 'T':
+    #     entry = models.TextField()
+    # elif entry_type == 'I':
+    #     entry = models.ImageField()
+    # elif entry_type == 'V':
+    #     entry = models.URLField()  # Web links to Youtube or else where
+    # elif entry_type == 'L':
+    #     entry = models.URLField()  # Web links to Youtube or else where
 
     def __str__(self):
         return str(self.last_modified) + ' : ' + self.entry
--- journal/views.py
+++ journal/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponseRedirect
 
 from journal.models import Student, Users, Activity
 from .forms import ActivityForm
+from .forms import EntryForm
 
 from datetime import datetime
 
@@ -69,3 +70,37 @@ def activity_form(request):
 
     return render(request, 'journal/activity_form.html',
                   {'form': form, 'student': curr_student.full_name()})
+
+
+@login_required
+def entry_form(request, activity_id, entry_id=None):
+    curr_student = Student.objects.get(email=request.user.email)
+    activity = Activity.objects.get(pk=activity_id)
+    if request.method == 'POST':
+        form = EntryForm(request.POST)
+        if form.is_valid():
+            f = form.save(commit=False)
+            f.stu_email = curr_student.email
+            f.activity_name = activity.activity_name
+            f.save()
+            activity.entries.add(f)
+            return HttpResponseRedirect('/activity/' + activity_id)
+    else:
+        form = EntryForm()
+
+    return render(request, 'journal/entry_form.html',
+                  {'form': form, 'activity': activity.activity_name})
+
+
+
+@login_required
+def entries(request, activity_id):
+    try:
+        activity = Activity.objects.get(id=activity_id)
+        name = activity.activity_name
+        activity_entries = activity.entries.all().reverse()
+    except ObjectDoesNotExist:
+        return render(request, 'journal/error.html')
+    return render(request, 'journal/entries.html',
+                  {'name': name, 'entries': activity_entries,
+                   'activity_id': activity.id})
