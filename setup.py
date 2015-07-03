f7e6149cabd0bd70db9470a6f73654ec85d3cd5b
Kevin Lee <leekevin@berkeley.edu>
2015-07-02 17:35:23 -0700

    more fixes

--- setup.py
+++ setup.py
@@ -9,14 +9,5 @@ setup(name='iU',
     author='Kevin Lee, Rodney Ho, Janice Zhao',
     author_email='',
     url='cas-codestig.rhcloud.com',
+    install_requires=['Django<=1.8'],
 )
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'openshift.settings'
-sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi', 'openshift'))
-virtenv = os.environ['APPDIR'] + '/virtenv/'
-os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python3.3/site-packages')
-virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
-try:
-    execfile(virtualenv, dict(__file__=virtualenv))
-except:
-    pass
