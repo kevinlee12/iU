a5e78e61f8f2de90370e986646195837b6a4aeba
Kevin Lee <leekevin@berkeley.edu>
2015-07-10 16:10:06 -0700

    beta entry backend

--- journal/views/entry.py
+++ journal/views/entry.py
@@ -43,8 +43,9 @@ def entries(request, activity_id):
     return render(request, 'journal/entries.html',
                   {'name': name, 'entries': activity_entries,
                    'activity_description': activity.activity_description,
-                   'activity_pk': activity.pk,
-                   'activity_id': activity.id})
+                   'activity_pk': activity.pk, 'activity_id': activity.id,
+                   'activity_start': activity.start_date,
+                   'activity_end': activity.end_date or "Ongoing"})
 
 
 @login_required
