6e4418157c348e30e50314d88d81bab11c8ac6b6
Kevin Lee <leekevin@berkeley.edu>
2015-08-01 19:11:22 -0700

    try 2 for circle

--- journal/migrations/0001_initial.py
+++ journal/migrations/0001_initial.py
@@ -9,6 +9,7 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('auth', '__first__'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('auth', '0006_require_contenttypes_0002'),
     ]
