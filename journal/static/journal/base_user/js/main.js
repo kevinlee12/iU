a58c8b6ec6a5728e065ffb55b4183c908b83213b
Janice <janicez348@gmail.com>
2015-08-07 23:40:57 -0700

    fix student and advisor forms

--- journal/static/journal/base_user/js/main.js
+++ journal/static/journal/base_user/js/main.js
@@ -39,18 +39,20 @@ function runEffect() {
   console.log("1");
   if ($(".main").css("left") == "240px") {
     $(".main").animate({
-      "left": "-=220px"
+      "left": "-=180px"
     }, 1000);
 
     $(".sidebar").animate({
       "left": "-=245px"
     }, 1000);
 
-    $(".button-2").css("display", "inline-block");
+    // $(".button-2").css("display", "inline-block");
+    $(".button-2").delay( 800 ).show(100);
+
   }
-  else if($(".main").css("left") == "20px") {
+  else if($(".main").css("left") == "60px") {
     $(".main").animate({
-      "left": "+=220px"
+      "left": "+=180px"
     }, 1000);
 
     $(".sidebar").animate({
