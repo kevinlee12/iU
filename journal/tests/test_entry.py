ed5f4d7a042e4bd9114a06c3da8b6e09cabe958c
Janice <janicez348@gmail.com>
2015-08-15 23:17:07 -0700

    more tests

--- journal/tests/test_entry.py
+++ journal/tests/test_entry.py
@@ -27,6 +27,9 @@ from selenium.webdriver.common.keys import Keys
 from django.conf import settings
 from django.core.management import call_command
 
+from django.conf import settings
+import os
+
 
 class EntrySeleleniumTests(StaticLiveServerTestCase):
     """Selenium tests for the entry form"""
@@ -61,7 +64,7 @@ class EntrySeleleniumTests(StaticLiveServerTestCase):
         self.selenium.quit()
         super()
 
-    def test_text_enty(self):
+    def test_text_entry(self):
         """Test to ensure that a student can add a text entry"""
         # Click on the first activity box: Walking around the block
         self.selenium.find_element_by_xpath('/html/body/div[1]/div[2]/div/div[1]/div/div[3]/a/div').click()
@@ -83,3 +86,126 @@ class EntrySeleleniumTests(StaticLiveServerTestCase):
         # Ensure that entry exists as the first box on the page.
         box_text = self.selenium.find_element_by_xpath('/html/body/div[1]/div[2]/div[2]/div[1]/a/div').text
         self.assertTrue(entry in box_text)
+
+    def test_image_entry(self):
+        """Test to ensure that a student can add an image entry"""
+        self.selenium.find_element_by_xpath('/html/body/div[1]/div[2]/div/div[1]/div/div[3]/a/div').click()
+        self.selenium.find_element_by_link_text('Add an entry').click()
+        # The following has 2 matching: Just walking and Adding entry...block
+        header_text = self.selenium.find_elements_by_tag_name('h3')[1].text
+        self.assertTrue('Adding entry for Walking around the block!' in header_text)
+        # Switching to iframe focus
+        self.selenium.switch_to_frame(self.selenium.find_element_by_id('id_entry_iframe'))
+        self.selenium.find_element_by_xpath('/html/body/div[2]/div[5]/div[3]/button[2]').click()
+        entry = 'http://images.jfdaily.com/jiefang/wenyu/new/201409/W020140919421426345484.jpg'
+        self.selenium.find_element_by_xpath('/html/body/div[2]/div[2]/div[1]/div/div/div[2]/div[2]/input')\
+            .send_keys(entry)
+        #click on the inset image button
+        self.selenium.find_element_by_xpath('/html/body/div[2]/div[2]/div[1]/div/div/div[3]/button').click()
+        # Switch back out of the iframe.
+        self.selenium.switch_to_default_content()
+        # Click on the submit button
+        self.selenium.implicitly_wait(10)
+        self.selenium.find_element_by_class_name('btn-success').click()
+        # Ensure that we are back on the entries page.
+        self.selenium.find_element_by_link_text('Add an entry')
+        # Ensure that entry exists on the page.
+        self.selenium.find_element_by_xpath("//img[@src='http://images.jfdaily.com/jiefang/wenyu/new/201409/W020140919421426345484.jpg']")
+
+    def test_video_entry(self):
+        """Test to ensure that a student can add an image entry"""
+        self.selenium.find_element_by_xpath('/html/body/div[1]/div[2]/div/div[1]/div/div[3]/a/div').click()
+        self.selenium.find_element_by_link_text('Add an entry').click()
+        # The following has 2 matching: Just walking and Adding entry...block
+        header_text = self.selenium.find_elements_by_tag_name('h3')[1].text
+        self.assertTrue('Adding entry for Walking around the block!' in header_text)
+        # Switching to iframe focus
+        self.selenium.switch_to_frame(self.selenium.find_element_by_id('id_entry_iframe'))
+        self.selenium.find_element_by_xpath('/html/body/div[2]/div[5]/div[3]/button[3]').click()
+        entry = 'https://www.youtube.com/watch?v=Rk_bV0RJRhs&index=20&list=PLJU_WCB1rA2SFwFy3lEvY_NH23ql1-Cgi'
+        self.selenium.find_element_by_xpath('/html/body/div[2]/div[2]/div[4]/div/div/div[2]/div/input')\
+            .send_keys(entry)
+        #click on the insert video button
+        self.selenium.find_element_by_xpath('/html/body/div[2]/div[2]/div[4]/div/div/div[3]/button').click()
+        # Switch back out of the iframe.
+        self.selenium.switch_to_default_content()
+        # Click on the submit button
+        self.selenium.implicitly_wait(10)
+        self.selenium.find_element_by_class_name('btn-success').click()
+        # Ensure that we are back on the entries page.
+        self.selenium.find_element_by_link_text('Add an entry')
+        self.selenium.find_element_by_xpath('//iframe[@src="//www.youtube.com/embed/Rk_bV0RJRhs"]')
+
+    # def test_image_video_text_entry(self):
+    #     """Test to ensure that a student can add image+text and video+text entries"""
+    #     self.selenium.find_element_by_xpath('/html/body/div[1]/div[2]/div/div[1]/div/div[3]/a/div').click()
+    #     self.selenium.find_element_by_link_text('Add an entry').click()
+    #     # The following has 2 matching: Just walking and Adding entry...block
+    #     header_text = self.selenium.find_elements_by_tag_name('h3')[1].text
+    #     self.assertTrue('Adding entry for Walking around the block!' in header_text)
+    #     # Switching to iframe focus
+    #     self.selenium.switch_to_frame(self.selenium.find_element_by_id('id_entry_iframe'))
+    #     # insert text
+    #     entry = 'I think I will bring my cat out next time.'
+    #     self.selenium.find_element_by_class_name('note-editable')\
+    #         .send_keys(entry)
+    #     #insert the image
+    #     self.selenium.find_element_by_xpath('/html/body/div[2]/div[5]/div[3]/button[2]').click()
+    #     image_path = os.path.join(settings.BASE_DIR, 'journal', 'tests', 'fixtures', 'test.png')
+    #     self.selenium.find_element_by_xpath('/html/body/div[2]/div[2]/div[1]/div/div/div[2]/div[1]/input').send_keys(image_path)
+    #     #click on the insert image button
+    #     self.selenium.find_element_by_xpath('/html/body/div[2]/div[2]/div[1]/div/div/div[3]/button').click()
+    #     # Switch back out of the iframe.
+    #     self.selenium.switch_to_default_content()
+    #     # Click on the submit button
+    #     self.selenium.implicitly_wait(10)
+    #     self.selenium.find_element_by_class_name('btn-success').click()
+    #     # Ensure that we are back on the entries page.
+    #     self.selenium.find_element_by_link_text('Add an entry')
+    #     #ensure the text is on the entries page
+    #     box_text = self.selenium.find_element_by_xpath('/html/body/div[1]/div[2]/div[2]/div[1]/a/div').text
+    #     self.assertTrue(entry in box_text)
+    #     #ensure the image is on the entries page
+    #     self.selenium.find_element_by_xpath('//<img src="/media/django-summernote/2015-08-15/5d55c61e-58df-427a-83a7-58d948b16619.png"]')
+
+    def test_all_three(self):
+        """Test to ensure that a student can add all three kinds of entry"""
+        # Click on the first activity box: Walking around the block
+        self.selenium.find_element_by_xpath('/html/body/div[1]/div[2]/div/div[1]/div/div[3]/a/div').click()
+        self.selenium.find_element_by_link_text('Add an entry').click()
+        # The following has 2 matching: Just walking and Adding entry...block
+        header_text = self.selenium.find_elements_by_tag_name('h3')[1].text
+        self.assertTrue('Adding entry for Walking around the block!' in header_text)
+        # Switching to iframe focus
+        self.selenium.switch_to_frame(self.selenium.find_element_by_id('id_entry_iframe'))
+        #insert text
+        text_entry = 'I think I will bring my cat out next time.'
+        self.selenium.find_element_by_class_name('note-editable')\
+            .send_keys(text_entry)
+        #insert image button
+        self.selenium.find_element_by_xpath('/html/body/div[2]/div[5]/div[3]/button[2]').click()
+        image_entry = 'http://images.jfdaily.com/jiefang/wenyu/new/201409/W020140919421426345484.jpg'
+        self.selenium.find_element_by_xpath('/html/body/div[2]/div[2]/div[1]/div/div/div[2]/div[2]/input')\
+            .send_keys(image_entry)
+        #click on the inset image button
+        self.selenium.find_element_by_xpath('/html/body/div[2]/div[2]/div[1]/div/div/div[3]/button').click()
+        #insert video button
+        self.selenium.find_element_by_xpath('/html/body/div[2]/div[5]/div[3]/button[3]').click()
+        video_entry = 'https://www.youtube.com/watch?v=Rk_bV0RJRhs&index=20&list=PLJU_WCB1rA2SFwFy3lEvY_NH23ql1-Cgi'
+        self.selenium.find_element_by_xpath('/html/body/div[2]/div[2]/div[4]/div/div/div[2]/div/input')\
+            .send_keys(video_entry)
+        #click on the insert video button
+        self.selenium.find_element_by_xpath('/html/body/div[2]/div[2]/div[4]/div/div/div[3]/button').click()
+        # Switch back out of the iframe.
+        self.selenium.switch_to_default_content()
+        # Click on the submit button
+        self.selenium.find_element_by_class_name('btn-success').click()
+        # Ensure that we are back on the entries page.
+        self.selenium.find_element_by_link_text('Add an entry')
+        # Ensure that entry exists as the first box on the page.
+        box_text = self.selenium.find_element_by_xpath('/html/body/div[1]/div[2]/div[2]/div[1]/a/div').text
+        self.assertTrue(text_entry in box_text)
+        self.selenium.find_element_by_xpath("//img[@src='http://images.jfdaily.com/jiefang/wenyu/new/201409/W020140919421426345484.jpg']")
+        self.selenium.find_element_by_xpath('//iframe[@src="//www.youtube.com/embed/Rk_bV0RJRhs"]')
+
+
